75040c70cdec08f9bfea7fa73edb9168ab53e273
Terra854 <bentinner@yahoo.com.sg>
2023-09-09 19:51:18 +0800

    Included 2D vectors and 3x3 Matrix

---
+++ GAM200/include/Vec2.h
@@ -0,0 +1,46 @@
+#pragma once
+class Vec2
+{
+public:
+	float x, y;
+
+	// Constructors
+	Vec2() : x(0.0f), y(0.0f) {}
+	Vec2(float _x, float _y);
+
+	// Do not change the following
+	Vec2& operator=(const Vec2& rhs) = default;
+	Vec2(const Vec2& rhs) = default;
+
+	// Assignment operators
+	Vec2& operator += (const Vec2& rhs);
+	Vec2& operator -= (const Vec2& rhs);
+	Vec2& operator *= (float rhs);
+	Vec2& operator /= (float rhs);
+
+	// Unary operators
+	Vec2 operator -() const;
+};
+
+// Binary operators
+Vec2 operator+ (const Vec2& lhs, const Vec2& rhs);
+Vec2 operator- (const Vec2& lhs, const Vec2& rhs);
+Vec2 operator* (const Vec2& lhs, float rhs);
+Vec2 operator* (float lhs, const Vec2& rhs);
+Vec2 operator/ (const Vec2& lhs, float rhs);
+
+Vec2 Vec2Normalize(const Vec2& pVec0);
+
+float Vec2Length(const Vec2& pVec0);
+
+//This function returns the square of pVec0's length. Avoid the square root
+float Vec2SquareLength(const Vec2& pVec0);
+
+float Vec2Distance(const Vec2& pVec0, const Vec2& pVec1);
+
+float Vec2SquareDistance(const Vec2& pVec0, const Vec2& pVec1);
+
+float Vec2DotProduct(const Vec2& pVec0, const Vec2& pVec1);
+
+float Vec2CrossProductMag(const Vec2& pVec0, const Vec2& pVec1);
+
---
+++ GAM200/src/Vec2.cpp
@@ -0,0 +1,108 @@
+#include "Vec2.h"
+#include <cmath>
+
+Vec2::Vec2(float _x, float _y)
+{
+	x = _x;
+	y = _y;
+}
+
+Vec2& Vec2::operator+=(const Vec2& rhs)
+{
+	x += rhs.x;
+	y += rhs.y;
+
+	return *this;
+}
+
+Vec2& Vec2::operator-=(const Vec2& rhs)
+{
+	x -= rhs.x;
+	y -= rhs.y;
+
+	return *this;
+}
+
+Vec2& Vec2::operator*=(float rhs)
+{
+	x *= rhs;
+	y *= rhs;
+
+	return *this;
+}
+
+Vec2& Vec2::operator/=(float rhs)
+{
+	x /= rhs;
+	y /= rhs;
+
+	return *this;
+}
+
+Vec2 Vec2::operator-() const
+{
+	return Vec2(-x, -y);
+}
+
+Vec2 operator+(const Vec2& lhs, const Vec2& rhs)
+{
+	return Vec2(lhs.x + rhs.x, lhs.y + rhs.y);
+}
+
+Vec2 operator-(const Vec2& lhs, const Vec2& rhs)
+{
+	return Vec2(lhs.x - rhs.x, lhs.y - rhs.y);
+}
+
+Vec2 operator*(const Vec2& lhs, float rhs)
+{
+	return Vec2(lhs.x * rhs, lhs.y * rhs);
+}
+
+Vec2 operator*(float lhs, const Vec2& rhs)
+{
+	return Vec2(lhs * rhs.x, lhs * rhs.y);
+}
+
+Vec2 operator/(const Vec2& lhs, float rhs)
+{
+	return Vec2(lhs.x / rhs, lhs.y / rhs);
+}
+
+Vec2 Vec2Normalize(const Vec2& pVec0)
+{
+	float f = Vec2Length(pVec0);
+	return Vec2(pVec0.x / f, pVec0.y / f);
+}
+
+float Vec2Length(const Vec2& pVec0)
+{
+	return sqrt(pVec0.x * pVec0.x + pVec0.y * pVec0.y);
+}
+
+float Vec2SquareLength(const Vec2& pVec0)
+{
+	return Vec2Length(pVec0) * Vec2Length(pVec0);
+}
+
+float Vec2Distance(const Vec2& pVec0, const Vec2& pVec1)
+{
+	Vec2 distance{ pVec1.x - pVec0.x, pVec1.y - pVec0.y };
+	return Vec2Length(distance);
+}
+
+float Vec2SquareDistance(const Vec2& pVec0, const Vec2& pVec1)
+{
+	Vec2 distance{ pVec1.x - pVec0.x, pVec1.y - pVec0.y };
+	return Vec2Length(distance) * Vec2Length(distance);
+}
+
+float Vec2DotProduct(const Vec2& pVec0, const Vec2& pVec1)
+{
+	return pVec1.x * pVec0.x + pVec1.y * pVec0.y;
+}
+
+float Vec2CrossProductMag(const Vec2& pVec0, const Vec2& pVec1)
+{
+	return pVec0.x * pVec1.y - pVec0.y * pVec1.x;
+}
