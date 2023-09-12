03948040bee246c482c429199d68b3f0b922a106
TehPiyoNoob <72617649+TehPiyoNoob@users.noreply.github.com>
2023-09-12 23:49:23 +0800

    Finished file read (untested)
    File reading should work correctly, but I have not properly test it

--- GAM200/src/Assets Manager/file_reader.h
+++ GAM200/src/Assets Manager/file_reader.h
@@ -1,7 +1,18 @@
+
+#include <vector>
+
+std::vector<std::vector<int>> mapdata;
+
 /*
-
-
-test
+This function is meant to read a .txt file and only intergers from it and save it into a vector<vector<int>>
+I plan to update them to be the serializer thing (maybe) taught in week 3 but ideally works for early on
+Returns true if successful
 */
+bool importMapFromFile(char* textfile);
+
+
+
+
+
 
 
