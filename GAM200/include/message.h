9d4eaf629b47e10121c8fabc4368059e63146ceb
zutilt10 <123jonathanwoo34@gmail.com>
2023-09-17 00:25:20 +0800

    Added base message class
    A new base message class from which all other message classes will inherit from has been added. I think how it works is that every game object is capable of sending its own kind of message

---
+++ GAM200/include/message.h
@@ -0,0 +1,29 @@
+#pragma once
+
+namespace MessageID
+{
+	enum MessageIDType
+	{
+		None,
+		Quit,
+		Collide,
+		MouseButton_Press,
+		MouseButton_Hold,
+		MouseMove,
+		Key_Press,
+		Key_Hold
+	};
+}
+
+//The message clas isn't meant to be used on it's own
+//Instead, every game object will be capable of sending it's own kind of
+//message and will inherit from this class in order to create them
+//The ID above will show what kind of signals we want our systems to send each other
+//More will be added as the game expands
+class Message
+{
+public:
+	Message(MessageID::MessageIDType id) : messageId(id) {};
+	MessageID::MessageIDType messageId;
+	virtual ~Message() {};
+};
