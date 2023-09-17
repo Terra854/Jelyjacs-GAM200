3f0a36e0a6a650fd77eb021ad3a91477e8f848b3
zutilt10 <123jonathanwoo34@gmail.com>
2023-09-17 22:31:42 +0800

    More Factory stuff
    Defined some of the functions in the class

--- GAM200/src/Factory.cpp
+++ GAM200/src/Factory.cpp
@@ -1,6 +1,63 @@
 #include "Factory.h"
 #include "Composition.h"
-#include "ComponentCreator.h"
 
-GameObjectFactory* gameObjectFactory = NULL;
+GameObjectFactory* gameObjFactory = NULL;
 
+GameObjectFactory::GameObjectFactory()
+{
+	gameObjectFactory = this;
+	lastGameObjID = 0;
+}
+
+GameObjectFactory::~GameObjectFactory()
+{
+
+}
+
+GOC* GameObjectFactory::createGameObj(std::string gameObjType)
+{
+
+}
+
+void GameObjectFactory::destroyGameObj(GOC* gameObject)
+{
+	gameObjsToBeDeleted.insert(gameObject);
+}
+
+void GameObjectFactory::destroyAllGameObjs()
+{
+	gameObjIDMap::iterator it = gameObjectMap.begin();
+	for (; it != gameObjectMap.end(); ++it)
+	{
+		delete it->second;
+	}
+
+	gameObjectMap.clear();
+}
+
+GOC* GameObjectFactory::createEmptyGameObj()
+{
+	GOC* gameObj = new GOC();
+	idGameObj(gameObj);
+	return gameObj;
+}
+
+void GameObjectFactory::idGameObj(GOC* gameObj)
+{
+	++lastGameObjID;
+	gameObj->ObjectId = lastGameObjID;
+	gameObjectMap[lastGameObjID] = gameObj;
+}
+
+GOC* GameObjectFactory::getObjWithID(GOC id)
+{
+	gameObjIDMap::iterator it = gameObjectMap.find(id);
+	if (it != gameObjectMap.end())
+	{
+		return it->second;
+	}
+	else
+	{
+		return NULL;
+	}
+}
