e805919998e88da0864f3791684f793aa5ce9018
zutilt10 <123jonathanwoo34@gmail.com>
2023-09-19 00:08:06 +0800

    Factory updated but...
    The factory is tentatively completed as in all of the essential functions are defined but for some reason, I'm having trouble using them in GameLogic.cpp

--- GAM200/src/GameLogic.cpp
+++ GAM200/src/GameLogic.cpp
@@ -2,9 +2,22 @@
 #include <iostream>
 #include "Assets Manager//file_reader.h"
 #include "ComponentType.h"
+#include "Factory.h"
+#include "ComponentCreator.h"
+#include "components/Transform.h"
+#include "ComponentType.h"
 
+GameLogic* LOGIC = NULL;
 
 void GameLogic::Initialize()
 {
-	
+	gameObjFactory->AddComponentCreator("Transform", new ComponentCreatorType<Transform>(ComponentTypeId::CT_Transform));
+
+	ComponentCreatorType<Transform> transformComponetCreator(ComponentTypeId::CT_Transform);
+	ComponentCreator* transformPtr = transformComponetCreator;
+
+	GameObjectFactory gameObjFactory;
+	std::string transform = "Transform";
+
+	gameObjFactory.AddComponentCreator(transform, transformPtr);
 }
