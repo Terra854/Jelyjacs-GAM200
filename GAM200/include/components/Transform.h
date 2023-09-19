ad6c934fd444ee318c08e0b6ba0e20c9b2fcb0cc
jiamingyeo <jiaming.yeo@digipen.edu>
2023-09-19 18:56:36 +0800

    added scale and matrix

--- GAM200/include/components/Transform.h
+++ GAM200/include/components/Transform.h
@@ -8,13 +8,14 @@ class Transform : public GameComponent
 public:
 	//centre position of bodies
 	Vec2 Position;
+	float Scale;
 	Vec2 PrevPosition;
 	float X_Velocity;
 	float X_Acceleration;
 	float Y_Velocity;
 	float Y_Acceleration;
 	float Mass;
-
+	Mat3 Matrix;
 	virtual void Initialize()
 	{
 
