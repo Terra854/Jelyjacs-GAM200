a7554980b500bd6e5a86c8176473d7d750531ad4
Terra854 <bentinner@yahoo.com.sg>
2023-11-02 15:27:49 +0800

    performance viewer is permanent

--- GAM200/src/LevelEditor.cpp
+++ GAM200/src/LevelEditor.cpp
@@ -72,7 +72,7 @@ void LevelEditor::DebugUniformGrid() {
 
 void LevelEditor::DebugPerformanceViewer() {
 	ImGui::SetNextWindowSize(ImVec2(0, 0));
-	ImGui::SetNextWindowPos(ImVec2(0, 30), ImGuiCond_Once);
+	//ImGui::SetNextWindowPos(ImVec2(0, 30), ImGuiCond_Once);
 	ImGui::Begin("DEBUG: Performance Viewer", &showPerformanceInfo);
 
 	for (std::pair<std::string, double> p : System_elapsed_time) {
@@ -1241,7 +1241,7 @@ void LevelEditor::Update() {
 			io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 	}
 
-	ImGui::ShowDemoWindow();
+	//ImGui::ShowDemoWindow();
 
 	if (ImGui::BeginMenuBar())
 	{
@@ -1288,7 +1288,9 @@ void LevelEditor::Update() {
 	}
 
 	showUniformGrid ? DebugUniformGrid() : DoNothing();
-	showPerformanceInfo ? DebugPerformanceViewer() : DoNothing();
+
+	//showPerformanceInfo ? DebugPerformanceViewer() : DoNothing();
+	DebugPerformanceViewer();
 
 	ListOfObjects();
 
