df06dd020c947a29a497a71cc4c92d791dd9373d
MNGChen <109013440+MNGChen@users.noreply.github.com>
2023-11-26 00:49:21 +0800

    new function for camera

--- GAM200/include/Graphic_gl/Camera.h
+++ GAM200/include/Graphic_gl/Camera.h
@@ -32,7 +32,7 @@ class Camera : public ISystems
 	virtual void Initialize();
 	virtual void Update();
 	virtual std::string SystemName() { return "Camera"; }
-
+	void SetCameraScale(Vec2 scale_input);
 	void SetToPlayer();
 
 	void toggleFreeCam() { free_cam = !free_cam; }
