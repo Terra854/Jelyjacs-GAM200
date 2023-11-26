b1f46bfdbb306211e9c42731d167f56fe8d1ed56
Terra854 <bentinner@yahoo.com.sg>
2023-11-27 00:44:44 +0800

    FIxed the zoomed out camera

--- GAM200/src/graphic_gl/Camera.cpp
+++ GAM200/src/graphic_gl/Camera.cpp
@@ -38,11 +38,10 @@ void Camera::Update() {
 				camera2D->scale = { 2.0f, 2.0f };
 			}
 			else {
-				position = { 0.0f, 0.0f };
 				scale = { 1.0f, 1.0f };
 			}
 		}
-		if(scale.x==1.0f && scale.y==1.0f)position = { 0.0f, 0.0f };
+		//if(scale.x==1.0f && scale.y==1.0f)position = { 0.0f, 0.0f };
 
 		SetToPlayer();
 
@@ -51,7 +50,7 @@ void Camera::Update() {
 
 	world_to_ndc = Mat3Scale(scale.x, scale.y) * Mat3Translate(position.x, position.y);
 	Vec2 window_scaling = {(float)window->width_init/(float)window->width,(float)window->height_init/(float)window->height};
-	world_to_ndc = Mat3Scale(window_scaling.x, window_scaling.y) * world_to_ndc;
+	//world_to_ndc = Mat3Scale(window_scaling.x, window_scaling.y) * world_to_ndc;
 
 }
 
@@ -79,29 +78,27 @@ void Camera::SetToPlayer() {
 	if (player != nullptr) {
 
 		// get the player's position
-		if (scale.x != 1.f || scale.y != 1.f) {
-			Transform* trans = static_cast<Transform*>(player->GetComponent(ComponentType::Transform));
+		Transform* trans = static_cast<Transform*>(player->GetComponent(ComponentType::Transform));
 
-			position.x = 0.f - trans->Position.x * 2.0f / window->width_init;
+		position.x = 0.f - trans->Position.x * 2.0f / window->width_init;
 
-			position.y = 0.f - trans->Position.y * 2.0f / window->height_init;
-			// make sure the camera is not out of bounds
-			/*
-			if (position.x > 0.6f - 1.f / scale.x) {
-				position.x = 0.6f - 1.f / scale.x;
-			}
-			else if (position.x < -0.6f + 1.f / scale.x) {
-				position.x = -0.6f + 1.f / scale.x;
-			}
-
-			if (position.y > 0.8f - 1.f / scale.y) {
-				position.y = 0.8f - 1.f / scale.y;
-			}
-			else if (position.y < -0.8f + 1.f / scale.y) {
-				position.y = -0.8f + 1.f / scale.y;
-			}
-			*/
+		position.y = 0.f - trans->Position.y * 2.0f / window->height_init;
+		// make sure the camera is not out of bounds
+		/*
+		if (position.x > 0.6f - 1.f / scale.x) {
+			position.x = 0.6f - 1.f / scale.x;
 		}
+		else if (position.x < -0.6f + 1.f / scale.x) {
+			position.x = -0.6f + 1.f / scale.x;
+		}
+
+		if (position.y > 0.8f - 1.f / scale.y) {
+			position.y = 0.8f - 1.f / scale.y;
+		}
+		else if (position.y < -0.8f + 1.f / scale.y) {
+			position.y = -0.8f + 1.f / scale.y;
+		}
+		*/
 
 	}
 }
