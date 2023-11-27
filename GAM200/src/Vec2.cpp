3d90497a4d19c457fcb8f34de762b6e63d111f04
Terra854 <bentinner@yahoo.com.sg>
2023-11-27 12:24:27 +0800

    comments

--- GAM200/src/Vec2.cpp
+++ GAM200/src/Vec2.cpp
@@ -10,6 +10,9 @@ This file contains the definitions of the member functions in the Vec2 class
 
 #include <cmath>
 
+/******************************************************************************
+	Add a vector to this vector
+*******************************************************************************/
 Vec2& Vec2::operator+=(const Vec2& rhs)
 {
 	x += rhs.x;
@@ -18,6 +21,9 @@ Vec2& Vec2::operator+=(const Vec2& rhs)
 	return *this;
 }
 
+/******************************************************************************
+	Subtract a vector from this vector
+*******************************************************************************/
 Vec2& Vec2::operator-=(const Vec2& rhs)
 {
 	x -= rhs.x;
@@ -26,6 +32,9 @@ Vec2& Vec2::operator-=(const Vec2& rhs)
 	return *this;
 }
 
+/******************************************************************************
+	Multiply a vector by a given magnitude
+*******************************************************************************/
 Vec2& Vec2::operator*=(float rhs)
 {
 	x *= rhs;
@@ -34,6 +43,9 @@ Vec2& Vec2::operator*=(float rhs)
 	return *this;
 }
 
+/******************************************************************************
+	Divide a vector by a given magnitude
+*******************************************************************************/
 Vec2& Vec2::operator/=(float rhs)
 {
 	x /= rhs;
@@ -42,92 +54,140 @@ Vec2& Vec2::operator/=(float rhs)
 	return *this;
 }
 
+/******************************************************************************
+	Returns a negative version of this vector
+*******************************************************************************/
 Vec2 Vec2::operator-() const
 {
 	return Vec2(-x, -y);
 }
 
-// Mostly for debugging via std::cout
+/******************************************************************************
+	Print this vector to a given output
+*******************************************************************************/
 std::ostream& operator<<(std::ostream& os, const Vec2& v)
 {
 	os << "(" << v.x << ", " << v.y << ")";
 	return os;
 }
 
+/******************************************************************************
+	Add 2 vectors together
+*******************************************************************************/
 Vec2 operator+(const Vec2& lhs, const Vec2& rhs)
 {
 	return Vec2(lhs.x + rhs.x, lhs.y + rhs.y);
 }
 
+/******************************************************************************
+	Subtracts a vector from another vector
+*******************************************************************************/
 Vec2 operator-(const Vec2& lhs, const Vec2& rhs)
 {
 	return Vec2(lhs.x - rhs.x, lhs.y - rhs.y);
 }
 
+/******************************************************************************
+	Multiply a given vector by a given magnitude
+*******************************************************************************/
 Vec2 operator*(const Vec2& lhs, float rhs)
 {
 	return Vec2(lhs.x * rhs, lhs.y * rhs);
 }
 
+/******************************************************************************
+	Multiply a given vector by a given magnitude
+*******************************************************************************/
 Vec2 operator*(float lhs, const Vec2& rhs)
 {
 	return Vec2(lhs * rhs.x, lhs * rhs.y);
 }
 
+/******************************************************************************
+	Divide a given vector by a given magnitude
+*******************************************************************************/
 Vec2 operator/(const Vec2& lhs, float rhs)
 {
 	return Vec2(lhs.x / rhs, lhs.y / rhs);
 }
 
-
+/******************************************************************************
+	Check to see if the 2 given vectors are equal
+*******************************************************************************/
 bool operator==(const Vec2& lhs, const Vec2& rhs)
 {
 	return (lhs.x == rhs.x && lhs.y == rhs.y);
 }
 
+/******************************************************************************
+	Check to see if the 2 given vectors are not equal
+*******************************************************************************/
 bool operator!=(const Vec2& lhs, const Vec2& rhs)
 {
 	return (lhs.x != rhs.x || lhs.y != rhs.y);
 }
 
-// Converts the vector for use in OpenGL calls
+/******************************************************************************
+	Converts the vector for use in OpenGL calls
+*******************************************************************************/
 glm::vec2 Vec2::ToGlmVec2() {
 	return glm::vec2(x, y);
 }
 
+/******************************************************************************
+	Normalize a given vector
+*******************************************************************************/
 Vec2 Vec2Normalize(const Vec2& v)
 {
 	float f = Vec2Length(v);
 	return Vec2(v.x / f, v.y / f);
 }
 
+/******************************************************************************
+	Calculates the length of a given vector
+*******************************************************************************/
 float Vec2Length(const Vec2& v)
 {
 	return sqrt(v.x * v.x + v.y * v.y);
 }
 
+/******************************************************************************
+	Calculates the square length of a given vector
+*******************************************************************************/
 float Vec2SquareLength(const Vec2& v)
 {
 	return Vec2Length(v) * Vec2Length(v);
 }
 
+/******************************************************************************
+	Calculates the distance between 2 vectors
+*******************************************************************************/
 float Vec2Distance(const Vec2& v0, const Vec2& v1)
 {
 	Vec2 distance{ v1.x - v0.x, v1.y - v0.y };
 	return Vec2Length(distance);
 }
 
+/******************************************************************************
+	Calculates the square distance between 2 vectors
+*******************************************************************************/
 float Vec2SquareDistance(const Vec2& v0, const Vec2& v1)
 {
 	Vec2 distance{ v1.x - v0.x, v1.y - v0.y };
 	return Vec2Length(distance) * Vec2Length(distance);
 }
 
+/******************************************************************************
+	Calculates the dot product of 2 vectors
+*******************************************************************************/
 float Vec2DotProduct(const Vec2& v0, const Vec2& v1)
 {
 	return v1.x * v0.x + v1.y * v0.y;
 }
 
+/******************************************************************************
+	Calculates the cross product magnitude of 2 vectors
+*******************************************************************************/
 float Vec2CrossProductMag(const Vec2& v0, const Vec2& v1)
 {
 	return v0.x * v1.y - v0.y * v1.x;
--- GAM200/src/Vec4.cpp
+++ GAM200/src/Vec4.cpp
@@ -8,6 +8,9 @@ This file contains the definitions of the member functions in the Vec4 class
 
 #include "Vec4.h"
 
+/******************************************************************************
+	Add a vector to this vector
+*******************************************************************************/
 Vec4& Vec4::operator+=(const Vec4& rhs)
 {
 	x += rhs.x;
@@ -18,6 +21,9 @@ Vec4& Vec4::operator+=(const Vec4& rhs)
 	return *this;
 }
 
+/******************************************************************************
+	Subtract a vector from this vector
+*******************************************************************************/
 Vec4& Vec4::operator-=(const Vec4& rhs)
 {
 	x -= rhs.x;
@@ -28,6 +34,9 @@ Vec4& Vec4::operator-=(const Vec4& rhs)
 	return *this;
 }
 
+/******************************************************************************
+	Multiply a vector by a given magnitude
+*******************************************************************************/
 Vec4& Vec4::operator*=(float rhs)
 {
 	x *= rhs;
@@ -38,6 +47,9 @@ Vec4& Vec4::operator*=(float rhs)
 	return *this;
 }
 
+/******************************************************************************
+	Divide a vector by a given magnitude
+*******************************************************************************/
 Vec4& Vec4::operator/=(float rhs)
 {
 	x /= rhs;
@@ -48,64 +60,98 @@ Vec4& Vec4::operator/=(float rhs)
 	return *this;
 }
 
+/******************************************************************************
+	Returns a negative version of this vector
+*******************************************************************************/
 Vec4 Vec4::operator-() const
 {
 	return Vec4(-x, -y, -z, -a);
 }
 
-// Mostly for debugging via std::cout
+/******************************************************************************
+	Print this vector to a given output
+*******************************************************************************/
 std::ostream& operator<<(std::ostream& os, const Vec4& v)
 {
 	os << "(" << v.x << ", " << v.y << ", " << v.z << ", " << v.a << ")";
 	return os;
 }
 
+/******************************************************************************
+	Add 2 vectors together
+*******************************************************************************/
 Vec4 operator+(const Vec4& lhs, const Vec4& rhs)
 {
 	return Vec4(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z, lhs.a + rhs.a);
 }
 
+/******************************************************************************
+	Subtracts a vector from another vector
+*******************************************************************************/
 Vec4 operator-(const Vec4& lhs, const Vec4& rhs)
 {
 	return Vec4(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z, lhs.a - rhs.a);
 }
 
+/******************************************************************************
+	Multiply 2 vectors together
+*******************************************************************************/
 Vec4 operator*(const Vec4& lhs, const Vec4& rhs)
 {
 	return Vec4(lhs.x * rhs.x, lhs.y * rhs.y, lhs.z * rhs.z, lhs.a * rhs.a);
 }
 
+/******************************************************************************
+	Multiply a given vector by a given magnitude
+*******************************************************************************/
 Vec4 operator*(const Vec4& lhs, float rhs)
 {
 	return Vec4(lhs.x * rhs, lhs.y * rhs, lhs.z * rhs, lhs.a * rhs);
 }
 
+/******************************************************************************
+	Multiply a given vector by a given magnitude
+*******************************************************************************/
 Vec4 operator*(float lhs, const Vec4& rhs)
 {
 	return Vec4(lhs * rhs.x, lhs * rhs.y, lhs * rhs.z, lhs * rhs.a);
 }
 
+/******************************************************************************
+	Divide a given vector by another vector
+*******************************************************************************/
 Vec4 operator/(const Vec4& lhs, const Vec4& rhs)
 {
 	return Vec4(lhs.x / rhs.x, lhs.y / rhs.y, lhs.z / rhs.z, lhs.a / rhs.a);
 }
 
+/******************************************************************************
+	Divide a given vector by a given magnitude
+*******************************************************************************/
 Vec4 operator/(const Vec4& lhs, float rhs)
 {
 	return Vec4(lhs.x / rhs, lhs.y / rhs, lhs.z / rhs, lhs.a / rhs);
 }
 
+/******************************************************************************
+	Check to see if the 2 given vectors are equal
+*******************************************************************************/
 bool operator==(const Vec4& lhs, const Vec4& rhs)
 {
 	return (lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z && lhs.a == rhs.a);
 }
 
+/******************************************************************************
+	Check to see if the 2 given vectors are not equal
+*******************************************************************************/
 bool operator!=(const Vec4& lhs, const Vec4& rhs)
 {
 	return (lhs.x != rhs.x && lhs.y != rhs.y && lhs.z != rhs.z && lhs.a != rhs.a);
 }
 
-// Converts the vector for use with ImGui
+/******************************************************************************
+	Converts the vector for use in ImGui
+*******************************************************************************/
 ImVec4 Vec4::ToImVec4() {
 	return ImVec4(x, y, z, a);
 }
