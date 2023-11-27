a3c43b8e28f7a9a61cf8560a016c467024142659
Terra854 <bentinner@yahoo.com.sg>
2023-11-27 19:39:57 +0800

    CTRL+ALT+DELETE and ALT+TAB

--- GAM200/include/EntryPoint.h
+++ GAM200/include/EntryPoint.h
@@ -6,16 +6,32 @@
 This file contains the main function that is used to run the game
 *//*__________________________________________________________________________*/
 #pragma once
+#include <windows.h>
 
 
 #ifdef GAM200_PLATFORM_WINDOWS
 	
 	extern Application* CreateApplication(); // To be defined in CLIENT
+
+	// Minimizes the game if it loses focus on fullscreen
+	// such as when alt tabbed or ctrl+alt+delete
+	// (Release build only)
+	LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+		switch (uMsg) {
+		case WM_KILLFOCUS:
+			// Minimize the window when focus is lost
+			ShowWindow(hwnd, SW_MINIMIZE);
+			break;
+		default:
+			return DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+		return 0;
+	}
+
 #if defined(DEBUG) | defined(_DEBUG)
 	int main() { // Entry point
 
 #else
-#include <windows.h>
 	// WinMain instead of main to not show the Win32 console
 	int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow) {
 
