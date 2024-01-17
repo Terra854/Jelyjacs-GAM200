831862facbea1e1a8e28ef246d5a9a4409bd14e9
wootube10 <w.jiahowjonathan@digipen.edu>
2024-01-17 09:27:03 +0800

    Layer stuff

--- GAM200/src/LevelEditor.cpp
+++ GAM200/src/LevelEditor.cpp
@@ -269,8 +269,9 @@ void LevelEditor::ObjectProperties() {
 		}
 	}
 	else {
+		ImGui::InputInt("Object Layer", &(object->layer));
 		ImGui::Text("Object ID: %d", object->GetId());
-		ImGui::Text("Object Layer: %d", object->GetLayer());
+		//ImGui::Text("Object Layer: %d", object->GetLayer());
 		ImGui::Text("Object Name: %s", object->GetName().c_str());
 		ImGui::Text("Number of components: %d", object->GetNumComponents());
 
