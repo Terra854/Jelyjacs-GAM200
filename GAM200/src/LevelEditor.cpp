cdfab3d83ccfb3f03f0355c401bbba76397c3516
TehPiyoNoob <72617649+TehPiyoNoob@users.noreply.github.com>
2024-02-05 19:14:23 +0800

    some change

--- GAM200/src/LevelEditor.cpp
+++ GAM200/src/LevelEditor.cpp
@@ -324,8 +324,8 @@ void LevelEditor::ObjectProperties() {
 				Object* o = objectFactory->cloneObject(object, 0, 64);
 				objectFactory->assignIdToObject(o);
 				selectedNum = o->GetId();
+				o->SetPrefab(object); // testing this line
 				cloneSuccessful = selectedNum;
-
 				l.second.second.push_back(o);
 			}
 		}
