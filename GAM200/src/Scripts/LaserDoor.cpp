002d1ebf903f3cafcb1325055c6167812f8a8a88
Wolfslayer03 <60912661+Wolfslayer03@users.noreply.github.com>
2024-03-06 10:34:01 +0800

    Laser Door intersecting with character and box

--- GAM200/src/Scripts/LaserDoor.cpp
+++ GAM200/src/Scripts/LaserDoor.cpp
@@ -10,38 +10,53 @@ This file contains the script for the laser "doors"
 #include <components/Animation.h>
 #include <components/Event.h>
 #include <Audio.h>
+#include <SceneManager.h>
 
-static float LaserOffSfxCooldown = 0.f;
-
-bool Intersects(Object* obj1, Object* obj2) {
-	Transform* obj1_t = static_cast<Transform*>(obj1->GetComponent(ComponentType::Transform));
-	Transform* obj2_t = static_cast<Transform*>(obj2->GetComponent(ComponentType::Transform));
-	Rectangular* obj1_b = static_cast<Rectangular*>(obj1->GetComponent(ComponentType::Body));
-	Rectangular* obj2_b = static_cast<Rectangular*>(obj2->GetComponent(ComponentType::Body));
-
-	if (obj1_t == nullptr || obj2_t == nullptr || obj1_b == nullptr || obj2_b == nullptr)
-		return false;
-
-	float obj1_x = obj1_t->Position.x;
-	float obj1_y = obj1_t->Position.y;
-	float obj1_w = obj1_b->width;
-	float obj1_h = obj1_b->height;
-
-	float obj2_x = obj2_t->Position.x;
-	float obj2_y = obj2_t->Position.y;
-	float obj2_w = obj2_b->width;
-	float obj2_h = obj2_b->height;
-
-    // Check if the two objects intersect
-	return (obj1_x < obj2_x + obj2_w &&
-        		obj1_x + obj1_w > obj2_x &&
-        		obj1_y < obj2_y + obj2_h &&
-        		obj1_y + obj1_h > obj2_y);
-}
-
-// Constructor for the LaserDoor class.
-// @param name: The name of the portal.
 namespace LaserDoor_Script {
+    static float LaserOffSfxCooldown = 0.f;
+    bool intersecting;
+
+    bool Intersects(Object* obj1, Object* obj2) {
+        Transform* obj1_t = static_cast<Transform*>(obj1->GetComponent(ComponentType::Transform));
+        Transform* obj2_t = static_cast<Transform*>(obj2->GetComponent(ComponentType::Transform));
+        Rectangular* obj1_b = static_cast<Rectangular*>(obj1->GetComponent(ComponentType::Body));
+        Rectangular* obj2_b = static_cast<Rectangular*>(obj2->GetComponent(ComponentType::Body));
+
+        if (obj1_t == nullptr || obj2_t == nullptr || obj1_b == nullptr || obj2_b == nullptr)
+            return false;
+
+        float obj1_x = obj1_t->Position.x;
+        float obj1_y = obj1_t->Position.y;
+        float obj1_w = obj1_b->width / 2;
+        float obj1_h = obj1_b->height / 2;
+
+        float obj2_x = obj2_t->Position.x;
+        float obj2_y = obj2_t->Position.y;
+        float obj2_w = obj2_b->width / 2;
+        float obj2_h = obj2_b->height / 2;
+
+        struct Point {
+            float x, y;
+        };
+
+        Point obj1_bl = { obj1_x - obj1_w, obj1_y - obj1_h };
+        Point obj1_tr = { obj1_x + obj1_w, obj1_y + obj1_h };
+
+        Point obj2_bl = { obj2_x - obj2_w, obj2_y - obj2_h };
+        Point obj2_tr = { obj2_x + obj2_w, obj2_y + obj2_h };
+
+        if (obj1_bl.x >= obj2_tr.x || obj1_tr.x <= obj2_bl.x)
+            return false;
+
+        if (obj1_bl.y >= obj2_tr.y || obj1_tr.y <= obj2_bl.y)
+            return false;
+
+        // Check if the two objects intersect
+        return true;
+    }
+
+    // Constructor for the LaserDoor class.
+    // @param name: The name of the portal.
     LaserDoor::LaserDoor(std::string name) : LogicScript(name)
     {
         std::cout << name << " Created" << std::endl;
@@ -52,6 +67,7 @@ namespace LaserDoor_Script {
     /*********************************************************************/
     void LaserDoor::Start(Object* obj) {
         std::cout << "LaserDoor Script Ready : " << obj->GetName() << std::endl;
+        bool intersecting = false;
     }
 
     /*********************************************************************/
@@ -74,19 +90,34 @@ namespace LaserDoor_Script {
         if (LaserDoor_a == nullptr || LaserDoor_b == nullptr)
             return;
 
-        std::cout << obj->GetName() << "'s : " << LaserDoor_a->frame_num << std::endl;
+        //std::cout << obj->GetName() << "'s : " << LaserDoor_a->frame_num << std::endl;
         // Disable collision only when the laser is off
         if (LaserDoor_b->active) {
             Object* Finn = objectFactory->FindObject("Finn");
             Object* Spark = objectFactory->FindObject("Spark");
             if (Finn != nullptr && Spark != nullptr) {
-                if (Intersects(Finn, obj) ) {
-
+                if (Intersects(Finn, obj)) {
+                    /*
+                    std::cout << "Finn is intersecting with " << obj->GetName() << std::endl;
+                    std::cout << "Finn's position : " << static_cast<Transform*>(Finn->GetComponent(ComponentType::Transform))->Position.x << ", " << static_cast<Transform*>(Finn->GetComponent(ComponentType::Transform))->Position.y << std::endl;
+                    std::cout << "Finn's width : " << static_cast<Rectangular*>(Finn->GetComponent(ComponentType::Body))->width << ", " << static_cast<Rectangular*>(Finn->GetComponent(ComponentType::Body))->height << std::endl;
+                    std::cout << "LaserDoor's position : " << static_cast<Transform*>(obj->GetComponent(ComponentType::Transform))->Position.x << ", " << static_cast<Transform*>(obj->GetComponent(ComponentType::Transform))->Position.y << std::endl;
+                   */
+                    intersecting = true;
                 }
                 if (Intersects(Spark, obj)) {
-
-				}
-			}
+                    intersecting = true;
+                }
+            }
+            for (auto& temp : objectFactory->FindAllObjectsByName("Box")) {
+                if (Intersects(temp, obj)) {
+                    intersecting = true;
+                }
+            }
+        }
+        if (intersecting) {
+            std::cout << "(Intersecting with )" << obj->GetName() << std::endl;
+            //SceneManager::RestartScene();
         }
         LaserDoor_b->active = static_cast<bool>(LaserDoor_a->frame_num != 2);
 
@@ -106,5 +137,4 @@ namespace LaserDoor_Script {
 
     // Creating an instance of LaserDoor.
     LaserDoor laserDoor("LaserDoor");
-}
-
+}// namespace LaserDoor_Script
