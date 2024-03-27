ebb9f38c9b63a0b900f07685cd7d34810861e8d9
Terra854 <bentinner@yahoo.com.sg>
2024-03-28 00:26:30 +0800

    Updated sounds for cutscenes

--- GAM200/src/Scripts/OpeningCutscene.cpp
+++ GAM200/src/Scripts/OpeningCutscene.cpp
@@ -18,6 +18,7 @@ This file contains the script for OpeningCutscene
 float OpeningCutsceneTime;
 Texture *opening_cutscene_1, *opening_cutscene_2, *opening_cutscene_3, *opening_cutscene_4, *opening_cutscene_5, *opening_cutscene_6;
 Text *opening_cutscene_text;
+FMOD::ChannelGroup *rain_sfx, *people_sfx;
 
 OpeningCutscene::OpeningCutscene(std::string name) : LogicScript(name)
 {
@@ -45,6 +46,11 @@ void OpeningCutscene::Start(Object* obj) {
 
 	OpeningCutsceneTime = 0.f;
 
+	audio->createChannelGroup("rain_bg", rain_sfx);
+	audio->createChannelGroup("people_in_store", people_sfx);
+	rain_sfx->setVolume(0.f);
+	people_sfx->setVolume(0.f);
+
 	std::cout << "OpeningCutscene Script Ready : " << obj->GetName() << std::endl;
 }
 
@@ -58,15 +64,20 @@ void OpeningCutscene::Update(Object* obj) {
 		//std::cout << "NIL OBJ : Player" << std::endl;
 		return;
 	}
-	
+
+	if (OpeningCutsceneTime == 0.f) {
+		audio->playSfx("rain_bg", rain_sfx);
+		audio->playSfx("people_in_store", people_sfx);
+	}
 	OpeningCutsceneTime += engine->GetDt();
 
 	opening_cutscene_1->opacity = opening_cutscene_2->opacity = opening_cutscene_3->opacity = opening_cutscene_4->opacity = opening_cutscene_5->opacity = opening_cutscene_6->opacity = 1.f;
 
-
 	if (OpeningCutsceneTime <= 2.f) {
 		opening_cutscene_1->opacity = OpeningCutsceneTime;
 		opening_cutscene_2->opacity = opening_cutscene_3->opacity = opening_cutscene_4->opacity = opening_cutscene_5->opacity = opening_cutscene_6->opacity = OpeningCutsceneTime - 1.f;
+		audio->setBackgroundVolume((OpeningCutsceneTime < 1.f ? OpeningCutsceneTime : 1.f));
+		rain_sfx->setVolume((OpeningCutsceneTime < 1.f ? OpeningCutsceneTime : 1.f));
 	}
 	else {
 		if (5.f <= OpeningCutsceneTime) {
@@ -76,6 +87,8 @@ void OpeningCutscene::Update(Object* obj) {
 			opening_cutscene_2->opacity = 11.f - OpeningCutsceneTime;
 		}
 		if (15.f <= OpeningCutsceneTime) {
+			rain_sfx->setVolume((16.f - OpeningCutsceneTime > 0.f ? 16.f - OpeningCutsceneTime : 0.f));
+			people_sfx->setVolume((OpeningCutsceneTime - 15.f < 1.f ? OpeningCutsceneTime - 15.f : 1.f));
 			opening_cutscene_3->opacity = 16.f - OpeningCutsceneTime;
 		}
 		if (20.f <= OpeningCutsceneTime) {
@@ -87,9 +100,11 @@ void OpeningCutscene::Update(Object* obj) {
 		if (30.f <= OpeningCutsceneTime) {
 			opening_cutscene_6->opacity = 31.f - OpeningCutsceneTime;
 			audio->setBackgroundVolume(31.f - OpeningCutsceneTime);
+			people_sfx->setVolume((31.f - OpeningCutsceneTime > 0.f ? 31.f - OpeningCutsceneTime : 0.f));
 		}
 		if (31.f < OpeningCutsceneTime) {
 			audio->setBackgroundAudio("main_menu_bg");
+			audio->stopSfx(rain_sfx);
 			SceneManager::LoadScene("main_menu.json");
 		}
 
