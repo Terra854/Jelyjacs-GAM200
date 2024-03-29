5e97e6c459d6368c5cd0b7697a1b52282d649f45
jiamingyeo <jiaming.yeo@digipen.edu>
2024-03-29 15:28:15 +0800

    added sound bar

---
+++ GAM200/src/Scripts/SoundBar.cpp
@@ -0,0 +1,72 @@
+
+#include "Scripts/SoundBar.h"
+#include <Utils.h>
+#include <Audio.h>
+#include <Factory.h>
+
+
+Object* sound_bg_obj;
+Transform* sound_bg_trans;
+
+
+// Constructor for the ButtonBase class.
+// @param name: The name of the portal.
+SoundBar::SoundBar(std::string name) : LogicScript(name)
+{
+    std::cout << name << " Created" << std::endl;
+}
+/*********************************************************************/
+// Start method called when the ButtonBase script is ready.
+// @param obj: The object to which this script is attached.
+/*********************************************************************/
+void SoundBar::Start(Object* obj) {
+    executeOnPause = true;
+    std::cout << "SoundBar Script Ready : " << obj->GetName() << std::endl;
+    sound_bg_obj = objectFactory->FindObject("SoundBar_bg");
+    
+}
+
+/*********************************************************************/
+// Update method called every frame to update the ButtonBase's logic.
+// @param obj: The object to which this script is attached.
+/*********************************************************************/
+void SoundBar::Update(Object* obj) {
+    if (obj == nullptr || !objectFactory->FindLayerThatHasThisObject(obj)->second.first.isVisible) {
+        return;
+    }
+
+    if (!isObjectClicked((Transform*)obj->GetComponent(ComponentType::Transform), Vec2(input::GetMouseX(), input::GetMouseY()))) {
+        return;
+    }
+
+
+    if (!sound_bg_obj)
+    {
+        std::cout << "object does not exist" << std::endl;
+        return;
+    }
+    sound_bg_trans = (Transform*)sound_bg_obj->GetComponent(ComponentType::Transform);
+
+    Transform* sound_bar_trans = (Transform*)obj->GetComponent(ComponentType::Transform);
+    float pos1 = sound_bar_trans->Position.x - sound_bar_trans->Scale.x / 2.0f;
+    if (input::IsPressedRepeatedly(KEY::mouseL))
+    {
+        sound_bg_trans->Scale.x = input::GetMouseX() - pos1;
+        sound_bg_trans->Position.x = pos1 + sound_bg_trans->Scale.x/2.0f;
+        audio->setMasterVolume(sound_bg_trans->Scale.x / sound_bar_trans->Scale.x);
+    }
+    
+
+}
+/*********************************************************************/
+// Shutdown method called when the ButtonBase script is being shut down.
+// @param obj: The object to which this script is attached.
+/*********************************************************************/
+void SoundBar::Shutdown(Object* obj) {
+    std::cout << "SoundBar Script Shutdown : " << obj->GetName() << std::endl;
+}
+
+// Creating an instance of ButtonBase.
+SoundBar soundbar("SoundBar");
+
+
