6f0c09914e7c22a1005361e2af6e4de3045e1b6f
jiamingyeo <jiaming.yeo@digipen.edu>
2024-04-06 18:01:45 +0800

    added how to play in main menu

--- GAM200/src/Scripts/MainMenuHowToPlay.cpp
+++ GAM200/src/Scripts/MainMenuHowToPlay.cpp
@@ -1,22 +1,13 @@
-/* !
-@file	MainMenuLoadGameButton.cpp
-@author
-@date	15/3/2023
 
-This file contains the script for the in-game clickable buttons
-*//*__________________________________________________________________________*/
-#include "Scripts/MainMenuLoadGameButton.h"
+#include "Scripts/MainMenuHowToPlay.h"
 #include <Utils.h>
 #include <Audio.h>
 #include <Factory.h>
-#include <Core_Engine.h>
-#include <SceneManager.h>
 #include <../components/Texture.h>
-#include <Object.h>
 
 // Constructor for the ButtonBase class.
 // @param name: The name of the portal.
-MainMenuLoadGameButton::MainMenuLoadGameButton(std::string name) : LogicScript(name)
+MainMenuHowToPlay::MainMenuHowToPlay(std::string name) : LogicScript(name)
 {
     std::cout << name << " Created" << std::endl;
 }
@@ -24,33 +15,49 @@ MainMenuLoadGameButton::MainMenuLoadGameButton(std::string name) : LogicScript(n
 // Start method called when the ButtonBase script is ready.
 // @param obj: The object to which this script is attached.
 /*********************************************************************/
-void MainMenuLoadGameButton::Start(Object* obj) {
+void MainMenuHowToPlay::Start(Object* obj) {
     executeOnPause = true;
-    std::cout << "ButtonBase Script Ready : " << obj->GetName() << std::endl;
 }
 
 /*********************************************************************/
 // Update method called every frame to update the ButtonBase's logic.
 // @param obj: The object to which this script is attached.
 /*********************************************************************/
-void MainMenuLoadGameButton::Update(Object* obj) {
-    if (obj == nullptr || !input::IsPressed(KEY::mouseL) || !objectFactory->FindLayerThatHasThisObject(obj)->second.first.isVisible) {
-        //std::cout << "NIL OBJ : ButtonBase" << std::endl;
+void MainMenuHowToPlay::Update(Object* obj) {
+    if (obj == nullptr || !objectFactory->FindLayerThatHasThisObject(obj)->second.first.isVisible) {
         return;
     }
 
-    if (isObjectClicked((Transform*)obj->GetComponent(ComponentType::Transform), Vec2(input::GetMouseX(), input::GetMouseY()))) {
-        std::cout << "Button Clicked" << std::endl;
-        audio->playSfx("button_click");
+    Transform* trans = (Transform*)obj->GetComponent(ComponentType::Transform);
+
+    if (!isObjectClicked(trans, Vec2(input::GetMouseX(), input::GetMouseY())))
+    {
+        return;
     }
+
+    if (input::IsPressed(KEY::mouseL))
+    {
+        audio->playSfx("button_click");
+        objectFactory->GetLayer("HowToPlayMenu")->second.first.isVisible = true;
+        objectFactory->GetLayer("MainMenu")->second.first.isVisible = false;
+           
+    }
+
+
+
+
+    
+
 }
 /*********************************************************************/
 // Shutdown method called when the ButtonBase script is being shut down.
 // @param obj: The object to which this script is attached.
 /*********************************************************************/
-void MainMenuLoadGameButton::Shutdown(Object* obj) {
-    std::cout << "ButtonBase Script Shutdown : " << obj->GetName() << std::endl;
+void MainMenuHowToPlay::Shutdown(Object* obj) {
+    std::cout << "MainMenuHowToPlay Script Shutdown : " << obj->GetName() << std::endl;
 }
 
 // Creating an instance of ButtonBase.
-MainMenuLoadGameButton mainMenuLoadGameButton("MainMenuLoadGameButton");
+MainMenuHowToPlay mainmenuhowtoplay("MainMenuHowToPlay");
+
+
