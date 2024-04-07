a615ea7455727f53c818230842f9189b9685c26a
jiamingyeo <jiaming.yeo@digipen.edu>
2024-04-07 21:09:03 +0800

    cleanup comments

--- GAM200/src/Scripts/ChatBoxLine1.cpp
+++ GAM200/src/Scripts/ChatBoxLine1.cpp
@@ -1,4 +1,10 @@
 #pragma once
+/* !
+@author Yeo Jia Ming
+@date	4/4/2024
+
+This file contains the script for chatbox text alignment
+*//*__________________________________________________________________________*/
 #include "Scripts/ChatBoxline1.h"
 #include <Utils.h>
 #include <Audio.h>
@@ -12,7 +18,6 @@
 
 
 void ChatBoxAlignment(Object* obj);
-void TextAlignment(Object* obj);
 
 
 namespace CHATBOX
@@ -28,25 +33,19 @@ namespace
     std::string dialogue_line;
 }
 
-// @param name: The name of the portal.
+
 ChatBoxLine1::ChatBoxLine1(std::string name) : LogicScript(name)
 {
     std::cout << name << " Created" << std::endl;
 }
-/*********************************************************************/
-// Start method called when the PauseButton script is ready.
-// @param obj: The object to which this script is attached.
-/*********************************************************************/
+
 void ChatBoxLine1::Start(Object* obj) {
     executeOnPause = true;
     std::cout << "ChatBoxline1 Script Ready : " << obj->GetName() << std::endl;
     
 }
 
-/*********************************************************************/
-// Update method called every frame to update the PauseButton's logic.
-// @param obj: The object to which this script is attached.
-/*********************************************************************/
+
 void ChatBoxLine1::Update(Object* obj) {
     if (obj == nullptr || !objectFactory->FindLayerThatHasThisObject(obj) || !objectFactory->FindLayerThatHasThisObject(obj)->second.first.isVisible) {
         return;
@@ -55,20 +54,16 @@ void ChatBoxLine1::Update(Object* obj) {
     text_obj->text = ::dialogue_line;
  
     ChatBoxAlignment(obj);
-    TextAlignment(obj);
     
 
 
 }
-/*********************************************************************/
-// Shutdown method called when the PauseButton script is being shut down.
-// @param obj: The object to which this script is attached.
-/*********************************************************************/
+
 void ChatBoxLine1::Shutdown(Object* obj) {
     std::cout << "ChatBoxline1 Script Shutdown : " << obj->GetName() << std::endl;
 }
 
-// Creating an instance of PauseButton.
+
 ChatBoxLine1 chatboxline1 ("ChatBoxLine1");
 
 
@@ -85,41 +80,6 @@ void ChatBoxAlignment(Object* obj)
     line_trans->Scale.x = top_right.x - top_left.x;
     line_trans->Position.x = top_left.x + line_trans->Scale.x / 2.0f;
     line_trans->Position.y = top_left.y;
-}
-
-void TextAlignment(Object* obj)
-{
-    /*
-    Text* text_obj = (Text*)obj->GetComponent(ComponentType::Text);
-    Transform* trans = (Transform*)obj->GetComponent(ComponentType::Transform);
-    int text_width = find_width(text_obj->text , text_obj->fontType , text_obj->fontSize);
-    int text_height = find_height(text_obj->text, text_obj->fontType, text_obj->fontSize);
-
-    std::string::size_type pos1{};
-    std::string::size_type pos2{ text_obj->text.size()};
-    std::string str_2 = text_obj->text;
-    std::string::size_type pos3{ str_2.find_first_of(' ') };
-    while (text_width > trans->Scale.x && pos3 != std::string::npos)
-    {
-        int new_text_width = text_width;
-        while (new_text_width > trans->Scale.x)
-        {
-            pos2 = str_2.find_last_of(' ', pos3 - 1);
-            if (pos2 == std::string::npos)
-            {
-                break;
-            }
-            new_text_width = find_width(str_2.substr(0,pos2), text_obj->fontType, text_obj->fontSize);
-            pos3 = pos2;
-        }
-        pos1 += pos3;
-        text_obj->text.at(pos1) = '\n';
-        str_2 = text_obj->text.substr(pos1, text_obj->text.size() - pos1);
-        text_width = find_width(str_2, text_obj->fontType,text_obj->fontSize);
-    }
-
-    std::cout << text_obj->text << std::endl;
-    */
 }
 
 void CHATBOX::change_text(std::string str)
